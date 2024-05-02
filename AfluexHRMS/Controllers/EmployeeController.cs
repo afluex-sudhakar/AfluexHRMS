b57dde6ebffe4dac626673886e3f0afea1321960
afluex-Tanishq <tanisk.afluex@gmail.com>
2024-05-02 18:33:04 +0530

    changes

---
+++ AfluexHRMS/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AfluexHRMS.Controllers
+{
+    public class EmployeeController : UserBaseController
+    {
+        // GET: Employee
+        public ActionResult EmployeeWork()
+        {
+            return View();
+        }
+    }
+}
